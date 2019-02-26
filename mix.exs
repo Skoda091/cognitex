f355b517dd5368d97672bda42053045ca2ce8184
Hans Kenis <hans.kenis@gmail.com>
2019-02-26 16:56:59 +0100

    Change elixir version.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cognitex.MixProject do
     [
       app: :cognitex,
       version: "0.1.0",
-      elixir: "~> 1.7.2",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
